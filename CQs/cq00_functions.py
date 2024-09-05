16e1d7378d3cc8f3e5ca53d531cf2a49a51aa664
seaguent <seaguent@unc.edu>
2024-09-05 15:50:15 -0400

    Finishing up assignments

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,17 @@
+"""My first challenge question in COMP 110!"""
+
+__author__ = "730741975"
+
+
+def mimic(message: str) -> str:
+    """returning message in this function"""
+    return message
+
+
+def main() -> None:
+    """ "This function pulls together functions"""
+    print(mimic(message=input("what is your message?")))
+
+
+if __name__ == "__main__":
+    main()
